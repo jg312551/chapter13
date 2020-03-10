66ced8ee125193ff99d0dcb895f83488a033d8c6
jg312551 <48728410+jg312551@users.noreply.github.com>
2020-03-10 12:39:42 -0500

    woops all update

---
+++ src/mediaFile/Movie.java
@@ -0,0 +1,45 @@
+package mediaFile;
+public class Movie {
+    private String title;
+    private int rating;
+    private int duration;
+
+
+    public Movie() {
+        title = "";
+        rating = 0;
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String t) {
+        title = t;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int r) {
+        rating = r;
+    }
+
+    public int getDuration() {
+        return rating;
+    }
+
+    public void setDuration(int d) {
+        duration = d;
+    }
+    public Movie(String title,int rating) {
+        this.title = title;
+        this.rating = rating;
+    }
+public void displayDuration(){
+     System.out.println("hours: " +(duration/60)+" minutes: "+(60 % duration));
+ }
+}
+
+
---
+++ src/mediaFile/Song.java
@@ -0,0 +1,59 @@
+package mediaFile;
+public class Song {
+    private String title;
+    private int rating;
+    private double price;
+    private boolean favorite;
+    private int numSongs=0;
+
+
+
+
+
+
+    public Song() {
+        title = "";
+        rating = 0;
+        price = 0;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String t) {
+        title = t;
+    }
+
+
+
+    public int getRating() {
+        return rating;
+    }
+    public void setRating(int r) {
+        rating = r;
+    }
+
+
+    public void setPrice(double p)
+    {
+        price =p;
+    }
+    public double getPrice(){
+        return price;
+    }
+
+    public void addToFavorites() {
+        favorite = true;
+    }
+
+
+    public Song(String title, double price,int rating) {
+        this.title = title;
+        this.price = price;
+        this.rating = rating;
+        ++numSongs;
+    }
+    public double getNumSongs(){
+        return numSongs;
+    }
+}
